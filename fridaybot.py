c02402b87897fe60d490fafa263978c4bbda44f5
Adam Scanga <sandmanscanga@gmail.com>
2022-05-28 19:30:47 -0400

    Added some linting best practice and also added a production lifecycle argument parser for handling the token

--- fridaybot.py
+++ fridaybot.py
@@ -1,4 +1,4 @@
-"""Module for running the FridayBot."""
+"""Module for running the FridayBot"""
 import sys
 import os
 import discord
@@ -7,21 +7,21 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix='friday ', intents=intents)
+client = commands.Bot(command_prefix="friday ", intents=intents)
 
 
 @client.event
 async def on_ready():
-    """Signals the 'bot is ready' event."""
+    """Signals the 'bot is ready' event"""
 
-    print('Bot is ready.')
+    print("[*] Bot is ready")
 
 
 @client.event
 async def on_member_join(member):
-    """Announces that a member has joined the server."""
+    """Announces that a member has joined the server"""
 
-    target_channel = 'test'
+    target_channel = "test"
     for channel in member.guild.channels:
         if str(channel) == target_channel:
             embed = discord.Embed(color=0x4a3d9a)
@@ -29,14 +29,29 @@ async def on_member_join(member):
             await channel.send(embed=embed)
             break
     else:
-        print(f'Could not find channel {target_channel}')
+        print(f"Could not find channel {target_channel}")
 
 
-if __name__ == '__main__':
-    try:
-        token = os.environ['FRIDAY_TOKEN']
-    except KeyError:
-        print('[!] Missing FRIDAY_TOKEN environment variable.')
-        sys.exit(1)
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--prod", action="store_true", required=False,
+        help="specify production lifecycle to automatically "\
+             "load bot token from system configuration"
+    )
+    args = parser.parse_args()
+    
+    if args.prod is True:
+        prod_config = "/etc/FridayBot/token.txt"
+        with open(prod_config, "r") as file:
+            token = file.read().strip()
     else:
-        client.run(token)
+        try:
+            token = os.environ['FRIDAY_TOKEN']
+        except KeyError:
+            print('[!] Missing FRIDAY_TOKEN environment variable.')
+            sys.exit(1)
+    
+    client.run(token)
